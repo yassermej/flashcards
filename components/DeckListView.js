f02714ae193b4dabada54293a3af54e39fc0b300
yassermej <yassermej50@gmail.com>
2017-11-01 20:48:48 +0100

    new component DeckViewList

---
+++ components/DeckListView.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Platform, StatusBar } from "react-native";
+import { TabNavigator, StackNavigator } from "react-navigation";
+import { teal, white } from "./utils/colors";
+
+export default class DeckListView extends React.Component {
+  render() {
+    return <View />;
+  }
+}
