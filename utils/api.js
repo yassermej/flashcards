409daba21370fabd9b43740bbc23cc32d62bdeca
yassermej <yassermej50@gmail.com>
2017-11-05 21:57:43 +0100

    modified file

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,6 @@ import { AsyncStorage } from "react-native";
 
 const DECK_STORAGE_KEY = "MobileFlashCards";
 
-
 export function fetchDecks() {
-    return AsyncStorage.getItem(DECK_STORAGE_KEY).then(data => json.parse(data));
-  }
+  return AsyncStorage.getItem(DECK_STORAGE_KEY).then(data => JSON.parse(data));
+}
