b94bbc0492e881904fa3670c7ee252fd687abe26
yassermej <yassermej50@gmail.com>
2017-11-06 21:05:01 +0100

    modified file

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,16 +1,87 @@
-import React from "react";
-import { View, Text } from "react-native";
+// @flow
 
-import { teal, white } from "../utils/colors";
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { fetchDecks } from "../actions";
+import { teal, white, gray } from "../utils/colors";
+
+import { setDummyData } from "../utils/_InitialData";
 
 class DeckListView extends React.Component {
-  render() {
+  componentDidMount() {
+    this.props.fetchDecks();
+  }
+
+  renderItem = ({ item }) => {
     return (
       <View>
-        <Text>hello deck</Text>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.cardCount}>{`${item.questions.length} cards`}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    const dummyData = {
+      React: {
+        title: "React",
+        questions: [
+          {
+            question: "What is React?",
+            answer: "A library for managing user interfaces"
+          },
+          {
+            question: "Where do you make Ajax requests in React?",
+            answer: "The componentDidMount lifecycle event"
+          }
+        ]
+      },
+      JavaScript: {
+        title: "JavaScript",
+        questions: [
+          {
+            question: "What is a closure?",
+            answer:
+              "The combination of a function and the lexical environment within which that function was declared."
+          }
+        ]
+      }
+    };
+
+    const decks = Object.values(dummyData);
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={decks}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => item.title}
+        />
       </View>
     );
   }
 }
 
-export default DeckListView;
+const mapStateToProps = state => ({
+  decks: state
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20
+  },
+  title: {
+    fontSize: 18,
+    textAlign: "center"
+  },
+  cardCount: {
+    color: gray,
+    textAlign: "center",
+    marginBottom: 15
+  }
+});
+
+export default connect(mapStateToProps, { fetchDecks })(DeckListView);
