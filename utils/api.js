2c3ed02645f8e92e5f5f37eb89016f37ecc01315
yassermej <yassermej50@gmail.com>
2017-11-07 21:08:58 +0100

    modified file

--- utils/api.js
+++ utils/api.js
@@ -5,3 +5,6 @@ const DECK_STORAGE_KEY = "MobileFlashCards";
 export function fetchDecks() {
   return AsyncStorage.getItem(DECK_STORAGE_KEY).then(data => JSON.parse(data));
 }
+
+export const addDeck = newDeck =>
+AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(newDeck)).then(data => newDeck)
