25985cd770374c39ae000d9e79dc78a55a90f47c
yassermej <yassermej50@gmail.com>
2017-11-10 20:26:21 +0100

    removed dummyData/render real data

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -23,33 +23,7 @@ class DeckListView extends React.Component {
   };
 
   render() {
-    const dummyData = {
-      React: {
-        title: "React",
-        questions: [
-          {
-            question: "What is React?",
-            answer: "A library for managing user interfaces"
-          },
-          {
-            question: "Where do you make Ajax requests in React?",
-            answer: "The componentDidMount lifecycle event"
-          }
-        ]
-      },
-      JavaScript: {
-        title: "JavaScript",
-        questions: [
-          {
-            question: "What is a closure?",
-            answer:
-              "The combination of a function and the lexical environment within which that function was declared."
-          }
-        ]
-      }
-    };
-
-    const decks = Object.values(dummyData);
+    const decks = Object.values(this.props.decks);
 
     return (
       <View style={styles.container}>
