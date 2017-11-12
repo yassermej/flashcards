664a8cba2b8820251856f735e6248165d7c116b2
yassermej <yassermej50@gmail.com>
2017-11-12 15:31:48 +0100

    Added new component: QuizView

---
+++ components/QuizView.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { teal, white, gray } from "../utils/colors";
+
+class QuizView extends Component {
+  render() {
+    return (
+      <View>
+        <Text>QuizView</Text>
+      </View>
+    );
+  }
+}
+
+export default QuizView;
