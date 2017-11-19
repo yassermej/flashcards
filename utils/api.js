a846fb98b4c20d92041823ceb7892484f31f77b9
yassermej <yassermej50@gmail.com>
2017-11-19 11:35:09 +0100

    feat: added function to clear all the decks

--- utils/api.js
+++ utils/api.js
@@ -2,6 +2,14 @@ import { AsyncStorage } from "react-native";
 
 const DECK_STORAGE_KEY = "MobileFlashCards";
 
+export const clearAll = () => {
+  AsyncStorage.clear()
+    .then(() => {})
+    .catch(e => {
+      console.log("Oh no, erro cleaning AsyncStorage!");
+    });
+};
+
 export function fetchDecks() {
   return AsyncStorage.getItem(DECK_STORAGE_KEY).then(data => JSON.parse(data));
 }
